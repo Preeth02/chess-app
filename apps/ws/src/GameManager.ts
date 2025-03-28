2dcdb1c09a04e4f1bbd47081364a907748371156
Preeth02 <preethampreetham27@gmail.com>
2025-03-28 19:34:24 +0530

    add the redis logic for storing pending users and getting the pending users

--- apps/ws/src/GameManager.ts
+++ apps/ws/src/GameManager.ts
@@ -1,44 +1,77 @@
 import Game from "./Game";
 import { WebSocket } from "ws";
 import { GAME_OVER, INIT_GAME, MAKE_MOVE } from "./messages";
+import { randomUUID } from "crypto";
+import { ExtendedWebSocket } from ".";
+import { Redis } from "ioredis";
 
 class GameManager {
   private games: Game[];
-  private pendingUser: WebSocket | null;
+  private pendingUser: Map<string, WebSocket>;
   private users: WebSocket[];
+  private redis: Redis;
 
   //TODO: Try to store all this in redis
 
-  constructor() {
+  constructor(redis: Redis) {
     this.games = [];
-    this.pendingUser = null;
+    this.pendingUser = new Map<string, WebSocket>();
     this.users = [];
+    this.redis = redis;
+  }
+
+  // Functions to interact with redis:
+  private async addPendingUser(userId: string) {
+    //   console.log("User object: ", user.id);
+    await this.redis.rpush("pending-users", userId);
+  }
+
+  private async getPendingUser(): Promise<string | null> {
+    const pendingUser = await this.redis.lpop("pending-users");
+    if (!pendingUser) {
+      console.log("No pending user in the DB");
+      return null;
+    }
+    return pendingUser;
   }
 
   addUser(ws: WebSocket) {
     this.users.push(ws);
-    this.handler(ws);
+    this.handler(ws as ExtendedWebSocket);
   }
 
   removeUser(ws: WebSocket) {
     this.users = this.users.filter((user) => user !== ws);
   }
 
-  handler(ws: WebSocket) {
-    ws.on("message", (data) => {
+  async handler(ws: ExtendedWebSocket) {
+    ws.on("message", async (data) => {
       const message = JSON.parse(data.toString());
-      if (message.type === INIT_GAME) {
-        if (!this.pendingUser) {
-          this.pendingUser = ws;
-        } else {
-          const toss = Math.floor(Math.random() * 2 + 1);
+
+      //Following code works until there is no logic in matchmaking based on the ratings
+
+      if (this.pendingUser.size === 0) {
+        this.pendingUser.set(ws.id, ws);
+        await this.addPendingUser(ws.id);
+      } else {
+        const toss = Math.floor(Math.random() * 2 + 1);
+        if (message.type === INIT_GAME) {
+          const pendingUserId = await this.getPendingUser();
+          if (!pendingUserId) {
+            return;
+          }
+          const userWs = this.pendingUser.get(pendingUserId);
+          if (!userWs) {
+            console.log("There's no pending user with the following user id");
+            return;
+          }
           let newGame;
           if (toss == 1) {
             if (message.payload.timer) {
-              newGame = new Game(this.pendingUser, ws, message.payload.timer);
+              newGame = new Game(userWs, ws, message.payload.timer);
             } else {
               newGame = new Game(
-                this.pendingUser,
+                userWs,
                 ws,
                 message.payload.timer,
                 message.payload.timer2
@@ -46,18 +79,17 @@ class GameManager {
             }
           } else {
             if (message.payload.timer) {
-              newGame = new Game(ws, this.pendingUser, message.payload.timer);
+              newGame = new Game(ws, userWs, message.payload.timer);
             } else {
               newGame = new Game(
                 ws,
-                this.pendingUser,
+                userWs,
                 message.payload.timer,
                 message.payload.timer2
               );
             }
           }
           this.games.push(newGame);
-          this.pendingUser = null;
         }
       }
       if (message.type === MAKE_MOVE) {
--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,14 +1,23 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 import GameManager from "./GameManager";
+import { randomUUID } from "crypto";
+import { Redis } from "ioredis";
+
+const redis = new Redis();
+
+export interface ExtendedWebSocket extends WebSocket {
+  id: string;
+}
 
 const wss = new WebSocketServer({ port: 8080 });
 
-const gameManager = new GameManager();
-wss.on("connection", function connection(ws) {
+const gameManager = new GameManager(redis);
+wss.on("connection", function connection(ws: ExtendedWebSocket) {
   ws.on("error", console.error);
+  ws.id = randomUUID();
   gameManager.addUser(ws);
   //   ws.send('something');
-  ws.on("close",()=>{
+  ws.on("close", () => {
     gameManager.removeUser(ws);
-  })
+  });
 });
