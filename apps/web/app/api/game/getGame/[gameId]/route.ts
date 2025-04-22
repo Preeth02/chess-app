460de214731e0f91c0f6a2732733b475966bcf1c
Preeth02 <preethampreetham27@gmail.com>
2025-04-22 16:27:39 +0530

    add get game route

---
+++ apps/web/app/api/game/getGame/[gameId]/route.ts
@@ -0,0 +1,21 @@
+import Apiresponse from "@lib/ApiResponse";
+import prisma from "@lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: NextResponse,
+  { params }: { params: { gameId: string } }
+) {
+  const gameId = params.gameId;
+  if (gameId.split("-").length != 5) {
+    return new Apiresponse(400, "Not a valid game ID");
+  }
+  const game = await prisma.gamePlayer.findFirst({
+    where: { gameID: gameId },
+    include: { game: true },
+  });
+  if (!game) {
+    return new Apiresponse(404, "No game with the following game id");
+  }
+  return new Apiresponse(200, "Game fetched successfully", game);
+}
